9862b0b4e2285cb2e411f31fc259b31c1f92535b
silvapasaribu <pasaribusilva46@gmail.com>
2023-06-17 20:52:05 +0700

    update profile

--- resources/views/landingpage/profile.blade.php
+++ resources/views/landingpage/profile.blade.php
@@ -9,7 +9,7 @@
 
 <div class="container"> 
   <div class="row">
-    <div class="col-lg-4">
+    <div class="col-lg-5">
       <div class="card mb-4">
         <div class="card-body text-center">
           @empty(Auth::user()->foto)
@@ -27,62 +27,27 @@
           @endempty
           <h5 class="my-3">{{ Auth::user()->name }}</h5>
           <div class="d-flex justify-content-center mb-2">
-            <a href="{{ url('/ubah_profil', $user->id) }}">
-              <button type="button" class="btn btn-primary rounded-pill-custom">Ubah Profil</button>
-            </a>
+            <a href="{{ url('/ubah_profil', $user->id) }}" class="btn btn-outline-primary rounded-pill-custom">
+              <i class="bi bi-pencil"></i>  
+              Ubah Profil
+          </a>
           </div>
        </div>
      </div>
-
-     <div class="card mb-4 mb-lg-0">
-      <div class="card-body p-0">
-        <div class="accordion accordion-flush" id="accordionFlushExample">
-          <h2 class="accordion-header" id="flush-headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-              <i class="fa fa-user fs-5"></i> <b>Akun Saya</b>
-            </button>
-          </h2>
-          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body">
-              <nav class="nav flex-column">
-                <a class="nav-link" href="#">Profil</a>
-                <a class="nav-link" href="#">Alamat</a>
-                <a class="nav-link" href="#">Ubah Password</a>
-              </nav>
-            </div>
-          </div>
-          
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                <i class="fa fa-shopping-bag fs-5"></i>
-                <b>Pesanan Saya</b>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                <i class="fa fa-bell fs-5"></i>
-                <b>Notifikasi</b>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
   </div>
   
-  <div class="col-lg-8">
+  <div class="col-lg-7">
     <div class="card mb-4">
       <div class="card-body">
         <div class="row">
           <div class="col-sm-3">
-            <h5 class="mb-0">Profil Saya</h5>
+            <h5 class="mb-0">&nbsp;Profil Saya</h5>
           </div>
         </div>
         <hr/>
         <div class="row">
           <div class="col-sm-3">
-            <p class="mb-0">Nama</p>
+            <p class="mb-0">&nbsp;Nama</p>
           </div>
           <div class="col-sm-9">
             <p class="text-muted mb-0">:&nbsp;{{ Auth::user()->name }}</p>
@@ -91,7 +56,7 @@
         <hr>
         <div class="row">
           <div class="col-sm-3">
-            <p class="mb-0">Email</p>
+            <p class="mb-0">&nbsp;Email</p>
           </div>
           <div class="col-sm-9">
             <p class="text-muted mb-0">:&nbsp;{{ Auth::user()->email }}</p>
@@ -100,7 +65,7 @@
         <hr>
         <div class="row">
           <div class="col-sm-3">
-            <p class="mb-0">Phone</p>
+            <p class="mb-0">&nbsp;Phone</p>
           </div>
           <div class="col-sm-9">
             @if (!empty(Auth::user()->hp))
